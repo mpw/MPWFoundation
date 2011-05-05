cc426dafb8eb1a36f21d1ada6636a7db1c283057
Marcel Weiher <marcel@metaobject.com>
2011-05-05 10:40:25 +0200

    MPWBlockInvocable ->  an object that can be called like a block

---
+++ MPWBlockInvocable.h
@@ -0,0 +1,32 @@
+//
+//  MPWBlockInvocable.h
+//  MPWFoundation
+//
+//  Created by Marcel Weiher on 5/4/11.
+//  Copyright 2011 metaobject ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+struct Block_descriptor {
+    unsigned long int reserved;
+    
+    /** Total size of the described block, including imported variables. */
+    unsigned long int size;
+    
+    /** Optional block copy helper. May be NULL. */
+    void (*copy)(void *dst, void *src);
+    
+    /** Optional block dispose helper. May be NULL. */
+    void (*dispose)(void *);
+};
+
+
+@interface MPWBlockInvocable : NSObject {
+	int	flags,reserved;
+    IMP invoke;
+	struct Block_descriptor *descriptor;
+}
+
+@end
---
+++ MPWBlockInvocable.m
@@ -0,0 +1,82 @@
+//
+//  MPWBlockInvocable.m
+//  MPWFoundation
+//
+//  Created by Marcel Weiher on 5/4/11.
+//  Copyright 2011 metaobject ltd. All rights reserved.
+//
+
+#import "MPWBlockInvocable.h"
+
+
+@implementation MPWBlockInvocable
+
+
+
+-(id)invokeWithArgs:(va_list)args
+{
+	return self;
+}
+
+
+static id blockFun( id self, ... ) {
+	va_list args;
+	va_start( args, self );
+	id result=[self invokeWithArgs:args];
+	va_end( args );
+	return result;
+}
+
+-(IMP)invokeMapper
+{
+	return (IMP)blockFun;
+}
+
+-init
+{
+	self=[super init];
+	if ( self ) {
+		invoke=(IMP)[self invokeMapper];
+	}
+	return self;
+}
+
+
+@end
+
+#if NS_BLOCKS_AVAILABLE
+
+#import "DebugMacros.h"
+
+@interface MPWBlockInvocableTest : MPWBlockInvocable
+{
+}
+@end
+
+
+@implementation MPWBlockInvocableTest
+
+typedef int (^intBlock)(int arg );
+
+
+-(id)invokeWithArgs:(va_list)args
+{
+	return (id)(va_arg( args, int ) * 3);
+}
+
++(void)testBlockInvoke
+{
+	id blockObj = [[[self alloc] init] autorelease];
+	INTEXPECT( ((intBlock)blockObj)( 3 ), 9, @"block(3) ");
+}
+
++testSelectors
+{
+	return [NSArray arrayWithObjects:
+					@"testBlockInvoke",
+			nil];
+}
+
+@end
+
+#endif
