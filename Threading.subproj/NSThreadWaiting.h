9b2be05f37de233e3b4a5d5033944d464f7cbb1c
Marcel Weiher <marcelw@microsoft.com>
2016-05-30 10:36:51 +0200

    busy-waiting for a condition

---
+++ Threading.subproj/NSThreadWaiting.h
@@ -0,0 +1,16 @@
+//
+//  NSThreadWaiting.h
+//  MPWFoundation
+//
+//  Created by Marcel Weiher on 30/05/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSThread(Waiting)
+
++ (BOOL)sleepForTimeInterval:(NSTimeInterval)ti orUnitlConditionIsMet:(BOOL (^)(void))conditionBlock;
+
+
+@end
---
+++ Threading.subproj/NSThreadWaiting.m
@@ -0,0 +1,59 @@
+//
+//  NSThreadWaiting.m
+//  MPWFoundation
+//
+//  Created by Marcel Weiher on 30/05/16.
+//
+//
+
+#import "NSThreadWaiting.h"
+#import "DebugMacros.h"
+
+@implementation NSThread(Waiting)
+
+#define CHECKS_PER_SECOND 100
+
++ (BOOL)sleepForTimeInterval:(NSTimeInterval)timeToWait orUnitlConditionIsMet:(BOOL (^)(void))conditionBlock
+{
+    NSTimeInterval end=[NSDate timeIntervalSinceReferenceDate] + timeToWait;
+    do {
+        if ( conditionBlock() ) {
+            return YES;
+        } else {
+            [self sleepForTimeInterval:1.0 / CHECKS_PER_SECOND];
+        }
+    } while ( [NSDate timeIntervalSinceReferenceDate] < end );
+    return NO;
+}
+
+@end
+
+
+@implementation NSThreadWaitingTests : NSThread
+
++(void)testTrueIsDoneImmediately
+{
+    NSTimeInterval start=[NSDate timeIntervalSinceReferenceDate];
+    [self sleepForTimeInterval:0.2 orUnitlConditionIsMet:^{ return YES; }];
+    NSTimeInterval end=[NSDate timeIntervalSinceReferenceDate];
+    EXPECTTRUE(end-start < 0.03 , @"wait time significantly less than timeout");
+}
+
++(void)testFalseWaitsFullTime
+{
+    NSTimeInterval start=[NSDate timeIntervalSinceReferenceDate];
+    [self sleepForTimeInterval:0.2 orUnitlConditionIsMet:^{ return NO; }];
+    NSTimeInterval end=[NSDate timeIntervalSinceReferenceDate];
+    EXPECTTRUE(end-start >= 0.2 , @"wait time greater than or equal timeout");
+}
+
++testSelectors
+{
+    return @[
+             @"testTrueIsDoneImmediately",
+             @"testFalseWaitsFullTime",
+             
+             ];
+}
+
+@end
