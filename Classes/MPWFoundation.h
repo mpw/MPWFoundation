54e73b332ace81eabacea66da77a7a179751baf0
Marcel Weiher <marcel@metaobject.com>
2018-09-02 13:21:48 +0200

    export all relevant headers

--- Classes/MPWFoundation.h
+++ Classes/MPWFoundation.h
@@ -60,6 +60,7 @@ THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #import "MPWFoundation/MPWStream.h"
+#import "MPWFoundation/MPWFilter.h"
 #import "MPWFoundation/MPWArrayFlattenStream.h"
 #import "MPWFoundation/MPWFlattenStream.h"
 #import "MPWFoundation/MPWByteStream.h"
