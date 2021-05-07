7699609f12be4dade1ad8be4e9bc51aa9c063730
Marcel Weiher <marcel@metaobject.com>
2021-05-07 11:44:57 +0200

    copy over test reporting improvements from ObjS

--- GNUstep/testmpwfoundation.m
+++ GNUstep/testmpwfoundation.m
@@ -59,36 +59,44 @@ static void runTests()
 
 	];
 
-	for (NSString *className in classes ) {
-		id testClass=NSClassFromString( className );
-		id fixture=testClass;
-		if ( fixture ) {
-			NSArray *testNames=[testClass testSelectors];
-			for ( NSString *testName in testNames ) {
-				if ( [testClass respondsToSelector:@selector(testFixture)] ) {
-					fixture=[testClass testFixture];
-				}
-				SEL testSel=NSSelectorFromString( testName );
-				@try {
-					tests++;
-					NSLog(@"%@:%@ -- will test",className,testName);
-					[fixture performSelector:testSel];
-					NSLog(@"%@:%@ -- success",className,testName);
-					success++;
-				} @catch (id error)  {
-					NSLog(@"\033[91;31m%@:%@ == error %@\033[0m",className,testName,error);
-					failure++;	
-				}
-			}
-		} else {
-			tests++;
-			failure++;
-			NSLog(@"\033[91;31m%@ == class not found\033[0m",className);
-		}
-
-	}
-	printf("\033[91;3%dmtests: %d total,  %d successes %d failures\033[0m\n",failure>0 ? 1:2,tests,success,failure);
-}
+    for (NSString *className in classes ) {
+        id testClass=NSClassFromString( className );
+        id fixture=testClass;
+        if ( fixture ) {
+            NSArray *testNames=[testClass testSelectors];
+            for ( NSString *testName in testNames ) {
+                if ( [testClass respondsToSelector:@selector(testFixture)] ) {
+                    fixture=[testClass testFixture];
+                }
+                SEL testSel=NSSelectorFromString( testName );
+                @try {
+                    tests++;
+                    fprintf(stderr,"                  %s:%s",[className UTF8String],[testName UTF8String]);
+                    //                    NSLog(@"%@:%@ -- will test",className,testName);
+                    [fixture performSelector:testSel];
+                    fprintf(stderr,"\r                                                                              \r");
+                    //                    NSLog(@"%@:%@ -- success",className,testName);
+                    success++;
+                } @catch (NSException *error)  {
+                    fprintf(stderr,"\r                                                                              \r");
+                    NSString *name=[error name];
+                    NSString *reason=[error reason];
+                    [failures addObject:[NSString stringWithFormat:@"%@:%@: %@",className,testName,reason]];
+                }
+            }
+        } else {
+            tests++;
+            [failures addObject:[NSString stringWithFormat:@"error: %@ not found",className]];
+        }
+        
+    }
+    printf("\n\033[91;3%dmtests: %d total,  %d successes %d failures\033[0m\n",failures.count > 0 ? 1:2,tests,success,failures.count);
+    if (failures.count) {
+        printf("\n");
+        for (NSString *failure in failures) {
+            printf("\033[91;31m%s\033[0m\n",[failure UTF8String]);
+        }
+    }}
 
 int main( int argc, char *argv[] ) {
 	MPWDictStore *a=[MPWDictStore store];
