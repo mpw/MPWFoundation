2202e2989ff115b52f4c22300ea97bc88c6e5449
Marcel Weiher <marcel@metaobject.com>
2022-06-06 10:26:59 +0200

    start of interacting with ssh programmatically, particularly scp, via libssh

---
+++ ObjectiveSSH/ObjectiveSSH.h
@@ -0,0 +1,18 @@
+//
+//  ObjectiveSSH.h
+//  ObjectiveSSH
+//
+//  Created by Marcel Weiher on 06.06.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjectiveSSH.
+FOUNDATION_EXPORT double ObjectiveSSHVersionNumber;
+
+//! Project version string for ObjectiveSSH.
+FOUNDATION_EXPORT const unsigned char ObjectiveSSHVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveSSH/PublicHeader.h>
+
+
