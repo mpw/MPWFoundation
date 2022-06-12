842b80cab90a6b233021eb574b12b8619c71d059
Marcel Weiher <marcel@metaobject.com>
2022-06-12 20:27:24 +0200

    use nil as data argument for at:put: to create directories in disk store (cripped from my sftp store)

--- Stores.subproj/MPWDiskStore.m
+++ Stores.subproj/MPWDiskStore.m
@@ -71,7 +71,12 @@
 -(void)at:(MPWGenericReference*)aReference put:(NSData*)theObject
 {
     NSError *error=nil;
-    BOOL success=[theObject writeToURL:[self fileURLForReference:aReference] options:NSDataWritingAtomic error:&error];
+    BOOL success=NO;
+    if ( theObject == nil ) {
+        success=[[NSFileManager defaultManager] createDirectoryAtURL:[self fileURLForReference:aReference] withIntermediateDirectories:NO attributes:nil error:&error];
+    } else {
+        success=[theObject writeToURL:[self fileURLForReference:aReference] options:NSDataWritingAtomic error:&error];
+    }
     if ( !success) {
         if (!error) {
             error=[NSError errorWithDomain:NSCocoaErrorDomain code:4 userInfo:@{}];
