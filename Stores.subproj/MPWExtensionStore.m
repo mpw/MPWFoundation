220037ea09132fbd620cfe4b6f24afe2ac91b062
Marcel Weiher <marcel@metaobject.com>
2023-04-01 19:21:52 +0200

    extension store also strips extensions

--- Stores.subproj/MPWExtensionStore.m
+++ Stores.subproj/MPWExtensionStore.m
@@ -14,12 +14,19 @@
 -(id<MPWReferencing>)mapReference:(id<MPWReferencing>)aReference
 {
     NSString *r=aReference.path;
-    return [r stringByAppendingPathExtension:self.extension];
+    id ref = [[aReference class] referenceWithPath:[[r stringByDeletingPathExtension] stringByAppendingPathExtension:self.extension]];
+    NSLog(@"mapped ref from '%@' to '%@'",aReference,ref);
+    return ref;
 }
 
 -(id<MPWReferencing>)reverseMapReference:(id<MPWReferencing>)aReference
 {
-    return [[aReference path] stringByDeletingPathExtension];
+    return [[aReference class] referenceWithPath:[[aReference path] stringByDeletingPathExtension]];
+}
+
+-(NSString*)description
+{
+    return [NSString stringWithFormat:@"<%@:%p: extension: %@ source: %@>",self.class,self,self.extension,self.source];
 }
 
 @end
@@ -29,21 +36,33 @@
 
 @implementation MPWExtensionStore(testing) 
 
-+(void)testExtensionAdded
++(instancetype)_testStore
 {
     NSDictionary *testDict=@{ @"index.html": @"hello world"};
     MPWDictStore *d=[MPWDictStore storeWithDictionary:testDict];
     EXPECTNIL( d[@"index"],@"shouldn't find without the extension")
     MPWExtensionStore *s=[self storeWithSource:d];
     s.extension = @"html";
-    IDEXPECT(s[@"index"],@"hello world",@"finds by adding extension");
-    
+    return s;
+}
+
++(void)testExtensionAdded
+{
+    IDEXPECT([self _testStore][@"index"],@"hello world",@"finds by adding extension");
+    EXPECTNIL([self _testStore][@"test"],@"key not there");
+}
+
++(void)testExtensionStrippedBeforeAdding
+{
+    IDEXPECT([self _testStore][@"index.html"],@"hello world",@"extension not added twice");
+    IDEXPECT([self _testStore][@"index.txt"],@"hello world",@"other extension stripped");
 }
 
 +(NSArray*)testSelectors
 {
    return @[
-			@"testExtensionAdded",
+       @"testExtensionAdded",
+       @"testExtensionStrippedBeforeAdding",
 			];
 }
 
