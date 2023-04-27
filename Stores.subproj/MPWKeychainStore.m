2d59339590caae17b4f1ce4f6be17018ad0521e8
Marcel Weiher <marcel@metaobject.com>
2023-04-27 20:17:39 +0200

    refactor: extract the conversion of the path to a Security Kit query dictionary into a separate method

--- Stores.subproj/MPWKeychainStore.m
+++ Stores.subproj/MPWKeychainStore.m
@@ -10,32 +10,8 @@
 
 @implementation MPWKeychainStore
 
-
--(void)at:(id<MPWReferencing>)aReference put:(id)theObject
+-(NSMutableDictionary*)queryDictForReference:(id<MPWReferencing>)aReference
 {
-    NSArray *path=[aReference relativePathComponents];
-    if ( [path.firstObject isEqual:@"password"] && path.count==3) {
-        NSString *server=path[1];
-        NSString *account=path[2];
-        NSDictionary *addQuery = @{
-            (id)kSecClass: (id)kSecClassInternetPassword,
-            (id)kSecAttrServer: [server retain],
-            (id)kSecAttrAccount: [account retain],
-            (id)kSecValueData: [[theObject asData] retain],
-        };
-        OSStatus status = SecItemAdd((__bridge CFDictionaryRef)addQuery, NULL);
-        if (status != errSecSuccess) {
-            NSLog(@"status: %d",status);
-            @throw [NSException exceptionWithName:@"keychainWriteError" reason:@"keychain error" userInfo:nil];
-        }
-    } else {
-        @throw [NSException exceptionWithName:@"keychainError" reason:@"invalid path error" userInfo:nil];
-    }
-}
-
--(id)at:(id<MPWReferencing>)aReference
-{
-    id retval=nil;
     NSArray *path=[aReference relativePathComponents];
     if ( [path.firstObject isEqual:@"password"] && path.count==3) {
         NSString *server=path[1];
@@ -44,43 +20,44 @@
             (id)kSecClass: (id)kSecClassInternetPassword,
             (id)kSecAttrServer: [server retain],
             (id)kSecAttrAccount: [account retain],
-            (id)kSecReturnData: @(true),
         };
-        OSStatus status = SecItemCopyMatching((CFDictionaryRef)query, (  CFTypeRef _Nullable*)&retval);
-        if ( status == -25300 ) {
-            return nil;
-        }
-        if (status != errSecSuccess) {
-
-            @throw [NSException exceptionWithName:@"keychainReadError" reason:@"keychain read error" userInfo:nil];
-        }
+        return [[query mutableCopy] autorelease];
     } else {
         @throw [NSException exceptionWithName:@"keychainError" reason:@"invalid path error" userInfo:nil];
+    }
+    return nil;
+}
+
+-(void)at:(id<MPWReferencing>)aReference put:(id)theObject
+{
+    NSMutableDictionary *addQuery=[self queryDictForReference:aReference];
+    addQuery[(id)kSecValueData]=[theObject asData];
+    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)addQuery, NULL);
+    if (status != errSecSuccess) {
+        NSLog(@"status: %d",status);
+        @throw [NSException exceptionWithName:@"keychainWriteError" reason:@"keychain error" userInfo:nil];
+    }
+}
+
+-(id)at:(id<MPWReferencing>)aReference
+{
+    NSMutableDictionary *query=[self queryDictForReference:aReference];
+    query[(id)kSecReturnData]=@(true);
+    id retval=nil;
+    OSStatus status = SecItemCopyMatching((CFDictionaryRef)query, (  CFTypeRef _Nullable*)&retval);
+    if (status != errSecSuccess && status != -25300) {
+
+        @throw [NSException exceptionWithName:@"keychainReadError" reason:@"keychain read error" userInfo:nil];
     }
     return retval;
 }
 
 -(void)deleteAt:(id<MPWReferencing>)aReference
 {
-    NSArray *path=[aReference relativePathComponents];
-    if ( [path.firstObject isEqual:@"password"] && path.count==3) {
-        NSString *server=path[1];
-        NSString *account=path[2];
-        NSDictionary *query = @{
-            (id)kSecClass: (id)kSecClassInternetPassword,
-            (id)kSecAttrServer: [server retain],
-            (id)kSecAttrAccount: [account retain],
-        };
-        OSStatus status = SecItemDelete((CFDictionaryRef)query);
-        if ( status == -25300 ) {
-            return ;
-        }
-        if (status != errSecSuccess) {
-            
-            @throw [NSException exceptionWithName:@"keychainReadError" reason:@"keychain read error" userInfo:nil];
-        }
-    } else {
-        @throw [NSException exceptionWithName:@"keychainError" reason:@"invalid path error" userInfo:nil];
+    OSStatus status = SecItemDelete((CFDictionaryRef)[self queryDictForReference:aReference]);
+    if (status != errSecSuccess &&  status != -25300) {
+        
+        @throw [NSException exceptionWithName:@"keychainReadError" reason:@"keychain read error" userInfo:nil];
     }
     return ;
 }
