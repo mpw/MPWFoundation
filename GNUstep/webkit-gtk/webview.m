3809efaf72753f682bae9a6e38514cb79d23fb1a
Marcel Weiher <marcel@metaobject.com>
2025-08-19 17:18:49 +0200

    try again to add webkit-gtk based start of HTMXNative for Linux

---
+++ GNUstep/webkit-gtk/webview.m
@@ -0,0 +1,28 @@
+#include <gtk/gtk.h>
+#include <webkit2/webkit2.h>
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    // Create main window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    // Create a WebKit web view
+    WebKitWebView *web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+
+    // Load a webpage
+    gchar *html="<html><body>Hello embedded</body></html>";
+    webkit_web_view_load_html (web_view, html , "file:///hello.html" );
+//    webkit_web_view_load_uri(web_view, "file:///hello.html");
+
+    // Add the web view to the window
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(web_view));
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
+}
+
