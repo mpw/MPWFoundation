8c96640c3d30e88a0136ee6ea2d0579337d44e8d
Marcel Weiher <marcel@metaobject.com>
2025-08-20 15:08:07 +0200

    copied gtk-web stuff to class and built a library from it

---
+++ GNUstep/webkit-gtk/Classes/ISWebRunner.h
@@ -0,0 +1,15 @@
+//
+// ISWebRunner.h
+//
+
+
+#import <MPWFoundation/MPWFoundation.h>
+
+@interface ISWebRunner : NSObject {
+
+}
+
+
+-(int)run:(int)argc args:(char **)argv;
+
+@end
---
+++ GNUstep/webkit-gtk/Interscript.h
@@ -0,0 +1,9 @@
+// 
+//  Interscript.h 
+//
+//  Framework for adapting HTML/HTMXNative on Linux
+//
+
+#import <MPWFoundation/MPWFoundation.h>
+#import <Interscript/ISWebRunner.h>
+
