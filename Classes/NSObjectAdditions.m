4360922f551988d3959f54f6584d1585b2f856c8
Marcel Weiher <marcel@metaobject.com>
2026-04-04 08:49:00 +0200

    refactor: move ivar lookup + class definition from ST to here

--- Classes/NSObjectAdditions.m
+++ Classes/NSObjectAdditions.m
@@ -217,6 +217,118 @@
     return [object_getClass(self) ivarNameAtOffset:(int)(address-(const void*)self) orIndex:index];
 }
 
+
+
+static void __collectInstanceVariables( Class aClass, NSMutableArray *varNames )
+{
+    int i;
+    if ( aClass ) {
+        unsigned int ivarCount=0;
+        Ivar  *ivars=NULL;
+        __collectInstanceVariables( [aClass superclass], varNames );
+        
+        ivars = class_copyIvarList(aClass, &ivarCount);
+        if ( ivars  && ivarCount > 0) {
+            for (i=0;i < ivarCount; i++ ){
+                Ivar ivar=ivars[i];
+                MPWInstanceVariableDefinition *varDescription;
+                MPWTypeDefinition *type=[MPWTypeDefinition descritptorForObjcCode:ivar_getTypeEncoding(ivar)[0]];
+                varDescription = [[[MPWInstanceVariableDefinition alloc] initWithName:[NSString stringWithCString:ivar_getName(ivar) encoding:NSASCIIStringEncoding]
+                                                                               offset:(int)ivar_getOffset(ivar)
+                                                                                 type:type]
+                                  autorelease];
+                [varNames addObject:varDescription];
+            }
+        }
+    }
+}
+
+
+
+static id ivarNameCache=nil;
+static id ivarsByClassAndName=nil;
+
+
+
++instanceVariables
+{
+    NSString *className = NSStringFromClass(self);
+    NSMutableArray *varNames;
+    if ( !ivarNameCache ) {
+        ivarNameCache = [[NSMutableDictionary alloc] init];
+    }
+    varNames = [ivarNameCache objectForKey:className];
+    if ( ! varNames  ) {
+        varNames = [NSMutableArray array];
+        __collectInstanceVariables( self, varNames );
+        [ivarNameCache setObject:varNames forKey:className];
+    }
+    return varNames;
+}
+
+
+
+static BOOL CreateClassDefinition( const char * name,
+                                  Class super_class, NSArray *variables  )
+{
+    Class class=objc_allocateClassPair(super_class,  name, 0);
+    for (MPWInstanceVariableDefinition* ivar in variables) {
+        class_addIvar(class, [[ivar name] cStringUsingEncoding:NSASCIIStringEncoding], sizeof(id), log2(sizeof(id)), [[ivar objcType] UTF8String]);
+        
+    }
+    objc_registerClassPair(class);
+    return YES;
+}
+
+
+
+#ifndef __clang_analyzer__
+// This 'leaks' because we are installing into the runtime, can't remove after
+
++(BOOL)createSubclassWithName:(NSString*)className instanceVariableArray:(NSArray*)vars
+{
+    int len= (int)[className length]+1;
+    char *class_name = malloc( len );
+    [className getCString:class_name maxLength:len encoding:NSASCIIStringEncoding];
+    return CreateClassDefinition( class_name, self , vars );
+}
+
++(BOOL)createSubclassWithName:(NSString*)className instanceVariables:(NSString*)varsAsString
+{
+    NSArray *varNames=[varsAsString componentsSeparatedByString:@" "];\
+    NSMutableArray *variableDefinitions=[NSMutableArray array];
+    for ( NSString *name in varNames ) {
+        MPWInstanceVariableDefinition *theVar=[[[MPWInstanceVariableDefinition alloc] initWithName:name offset:0 type:[MPWTypeDefinition descritptorForObjcCode:'@']] autorelease];
+        [variableDefinitions addObject:theVar];
+    }
+    return [self createSubclassWithName:className instanceVariableArray:variableDefinitions];
+}
+
+#endif
+
++(BOOL)createSubclassWithName:(NSString*)className
+{
+    return [self createSubclassWithName:className instanceVariables:@""];
+}
+
+
++(MPWInstanceVariableDefinition*)ivarForName:(NSString*)name
+{
+    id className = NSStringFromClass(self);
+    id instVarDict=nil;
+    if ( !ivarsByClassAndName ) {
+        ivarsByClassAndName=[NSMutableDictionary new];
+    }
+    instVarDict = [ivarsByClassAndName objectForKey:className];
+    if ( !instVarDict ) {
+        id ivarDefs = [self instanceVariables];
+        id names = [[ivarDefs collect] name];
+        instVarDict=[NSMutableDictionary dictionaryWithObjects:ivarDefs forKeys:names];
+        [ivarsByClassAndName setObject:instVarDict forKey:className];
+    }
+    return [instVarDict objectForKey:name];
+}
+
 @end
 
 @implementation NSObject(methodInstallation)
